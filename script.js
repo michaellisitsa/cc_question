8d09015a8e841a10a67d06fc98029f34e8ad6ba2
Michael Lisitsa <michaellisitsa@Michaels-MacBook-Pro-2.local>
2021-10-11 22:40:25 +1100

    Major refactor to use classes - Modal class only contains logic for rendering the modal and removing it. - Modal class receives params for message and what it should do on dismissal - Prevent fetching DOM nodes both in render func and dismiss func, by use of this - Change to using id for javascript dom node fetching - Prevent having to add and remove event listeners, because they are bound to the specific instance of the button in the Modal class - Return message lives in a permanent DOM element rather than recreated each time.

--- script.js
+++ script.js
@@ -1,90 +1,83 @@
-var modal;
+class Modal {
+  constructor(message, onClick){
+    this.message = message;
+    this.onClick = onClick;
+  }
+  loadModal(){
+    var body = document.getElementsByTagName('body')[0];
 
-const loadModal = (message) => {
-  //https://www.w3schools.com/jsref/dom_obj_body.asp
-  var body = document.getElementsByTagName('body')[0];
+    //Create the modal parent tag
+    const modal = document.createElement('div');
+    modal.setAttribute('id', 'myModal');
+    modal.setAttribute('class', 'modal');
+    body.appendChild(modal);
 
-  //Create the modal parent tag
-  modal = document.createElement('div');
-  modal.setAttribute('id', 'myModal');
-  modal.setAttribute('class', 'modal');
-  body.appendChild(modal);
+    //create and append the content wrapper of the modal
+    var modalContent = document.createElement('div'); // Create a text node
+    modalContent.setAttribute('class', 'modal-content');
+    modal.appendChild(modalContent);
 
-  //create and append the content wrapper of the modal
-  var modalContent = document.createElement('div'); // Create a text node
-  modalContent.setAttribute('class', 'modal-content');
-  modal.appendChild(modalContent);
+    // Create a text dialogue within the modal. Use class for styling the text.
+    var confirmationText = document.createElement('p');
+    confirmationText.innerText = this.message;
+    modalContent.appendChild(confirmationText);
 
-  // Create a text dialogue within the modal. Use class for styling the text.
-  var confirmationText = document.createElement('p');
-  confirmationText.innerText = message;
-  modalContent.appendChild(confirmationText);
+    //Create Yes  buttons inside the dialogue
+    var yesBtn = document.createElement('button');
+    yesBtn.setAttribute('class', 'yes');
+    yesBtn.innerText = 'Yes';
+    modalContent.appendChild(yesBtn);
 
-  //Create Yes  buttons inside the dialogue
-  var yesBtn = document.createElement('button');
-  yesBtn.setAttribute('class', 'yes');
-  yesBtn.innerText = 'Yes';
-  modalContent.appendChild(yesBtn);
-
-  //Create No buttons inside the dialogue
-  var noBtn = document.createElement('button');
-  noBtn.setAttribute('class', 'no');
-  noBtn.innerText = 'No';
-  modalContent.appendChild(noBtn);
+    //Create No buttons inside the dialogue
+    var noBtn = document.createElement('button');
+    noBtn.setAttribute('class', 'no');
+    noBtn.innerText = 'No';
+    modalContent.appendChild(noBtn);
 
 
-  //Add event listeners on the buttons with class "yes" and "no"
-  yesBtn.addEventListener('click', yesListener);
-  noBtn.addEventListener('click', noListener);
-};
+    //Add event listeners on the buttons with class "yes" and "no"
+    yesBtn.addEventListener('click', () => {
+      this.onClick("you just clicked Yes");
+      this.remove();
+    });
+    noBtn.addEventListener('click', () => {
+      this.onClick("you just clicked No");
+      this.remove();
+    });
 
-function yesListener (e) {
-  // if (!e.target.matches('.yes')) return;
-  // e.preventDefault();
-  //Remove event listeners 
-  //https://stackoverflow.com/questions/4402287/javascript-remove-event-listener#comment12343620_4402287
-  // document.removeEventListener('click',yesListener)
-  // document.removeEventListener('click',noListener)
-  dismiss('You just clicked "Yes"');
-};
-
-function noListener (e) {
-  // if (!e.target.matches('.no')) return;
-  // e.preventDefault();
-  // document.removeEventListener('click',noListener)
-  // document.removeEventListener('click',yesListener)
-
-  dismiss('You just clicked "No"');
-};
-
-//gomakethings.com/listening-for-click-events-with-vanilla-javascript
-// Action on clicking function on button on main page (not modal)
-// Adjusted to avoid anonymous function
-// https://ultimatecourses.com/blog/avoiding-anonymous-javascript-functions
-function btnClick(e) {
-  // Bail if the correct class isn't found on the clicked element
-  if (!e.target.matches('.confirmation')) return;
-  e.preventDefault();
-
-  //Remove any previous return messages
-  const returnMessage = document.querySelector('.return');
-  if (returnMessage != null) {
-    returnMessage.remove();
+    this.modalElement = modal;
   }
 
-  //https://developer.mozilla.org/en-US/docs/Learn/HTML/Howto/Use_data_attributes
-  //How to pass arbitrary data with component
-  loadModal(e.target.dataset.message);
+  remove(){
+    this.modalElement.remove();
+  }
+
 }
 
-document.addEventListener('click', btnClick);
+// Which DOM node receives the return message 
+const returnMessageEl = document.querySelector('#return');
 
-const dismiss = (message) => {
-  let modal = document.getElementById('myModal');
-  var body = document.getElementsByTagName('body')[0];
-  modal.remove();
-  var returnMessage = document.createElement('p');
-  returnMessage.setAttribute('class', 'return');
-  returnMessage.innerText = message;
-  body.appendChild(returnMessage);
+// What to do when modal is dismissed
+const setReturnMsg = (message) => {
+  returnMessageEl.innerText = message;
 };
+
+// What to do when user clicks a button
+function btnClick(modal) {
+  return ()=>{
+  modal.loadModal();
+  }
+}
+
+// Attach modal event to each button
+const modal1 = new Modal('Do you want to confirm, yes or no?', setReturnMsg);
+const button1 = document.getElementById('confirmation1');
+button1.addEventListener('click', btnClick(modal1));
+
+const modal2 = new Modal('Do you want to confirm, Ja or Nein?', setReturnMsg);
+const button2 = document.getElementById('confirmation2');
+button2.addEventListener('click', btnClick(modal2));
+
+
+
+
